1faf7061d699a28d03403ee73c6d989d1329c81c
Your Name <you@example.com>
2022-07-27 13:33:36 +0800

    test2

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -2,7 +2,7 @@
 	<div class="home-page">
 		<div class="banner">
 			<div class="container">
-				<h1 class="logo-font">alvin-deploy</h1>
+				<h1 class="logo-font">alvin-deploy-1</h1>
 				<p>A place to share your knowledge.</p>
 			</div>
 		</div>
